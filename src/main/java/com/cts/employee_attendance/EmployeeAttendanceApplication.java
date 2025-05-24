a496b51d0a4759ff4011abdc6e038773a4713459
darkwizz-cts <mohamedakram.mohamednazir@cognizant.com>
2025-05-24 14:23:11 +0530

    Add ModelMapper

--- src/main/java/com/cts/employee_attendance/EmployeeAttendanceApplication.java
+++ src/main/java/com/cts/employee_attendance/EmployeeAttendanceApplication.java
@@ -1,7 +1,9 @@
 package com.cts.employee_attendance;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class EmployeeAttendanceApplication {
@@ -10,4 +12,8 @@ public class EmployeeAttendanceApplication {
 		SpringApplication.run(EmployeeAttendanceApplication.class, args);
 	}
 
+	@Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
+	}
 }
