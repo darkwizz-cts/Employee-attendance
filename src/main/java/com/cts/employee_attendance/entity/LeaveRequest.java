c38aaf4498c0cd17470d62e8c5846844bfd753d1
darkwizz-cts <mohamedakram.mohamednazir@cognizant.com>
2025-05-24 14:11:32 +0530

    Add constructors to entities

--- src/main/java/com/cts/employee_attendance/entity/LeaveRequest.java
+++ src/main/java/com/cts/employee_attendance/entity/LeaveRequest.java
@@ -4,14 +4,15 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
-enum LeaveType{
-    
-}
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class LeaveRequest {
     @Id
